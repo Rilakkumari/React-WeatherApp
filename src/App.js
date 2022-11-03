2229de32e648112146d2b2388757bf0c0df8aa7d
Rilakkumari <kubotamarlene@gmail.com>
2022-11-03 20:56:45 +0900

    updated to Weather app.
    Looks like HW week 3 now!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles.css";
+import React from "react";
+import Search from "./Search.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <h1>Weather App</h1>
+      <Search />
     </div>
   );
 }
-
-export default App;
