68713e2fc971826e9d96c29f6ed50120108e7d5e
Rilakkumari <kubotamarlene@gmail.com>
2022-11-15 09:21:33 +0900

    changed CSS of icon a bit and added alt text

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ export default function App() {
           <div className="row">
             <div className="col-4">
               <h2 className="currentcity">{weatherData.city}</h2>
-              <img className="currentCityIcon" src={weatherData.iconUrl} />
+              <img alt="icon" className="currentCityIcon" src={weatherData.iconUrl} />
             </div>
             <div className="col-4">
               <h3 className="currenttime">
