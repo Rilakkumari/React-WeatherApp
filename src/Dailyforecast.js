ed1e53aa1962af66707e897b2cf910cf6db33133
Rilakkumari <kubotamarlene@gmail.com>
2022-11-18 10:18:41 +0900

    added base JS for weatherforecast

--- src/Dailyforecast.js
+++ src/Dailyforecast.js
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Dailyforecast() {
-  return (
-    <div className="row dailyforecast">
-      <div className="col-2">
-        Tue <img src="pictures/039-sun.png" alt="" className="icons" />
-        <strong>23°</strong> 16°
+
+export default function Dailyforecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    let apiKey = "d954e13a4e22b470136cf62da9402c50";
+    let longitude = props.cordinate.lon;
+    let latitude = props.cordinate.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  } else {
+    return (
+      <div className="row dailyforecast">
+        <div className="col-2">
+          Tue <img src="pictures/039-sun.png" alt="" className="icons" />
+          <strong>23°</strong> 16°
+        </div>
+        <div className="col-2">
+          Wed <img src="pictures/038-cloudy-3.png" alt="" className="icons" />
+          <strong>12°</strong> 14°
+        </div>
+        <div className="col-2">
+          Thu <img src="pictures/011-cloudy.png" alt="" className="icons" />
+          <strong>21°</strong> 17°
+        </div>
+        <div className="col-2">
+          Fri <img src="pictures/034-cloudy-1.png" alt="" className="icons" />
+          <strong>24°</strong> 19°
+        </div>
+        <div className="col-2">
+          Sat <img src="pictures/039-sun.png" alt="" className="icons" />
+          <strong>26°</strong> 21°
+        </div>
+        <div className="col-2">
+          Sun <img src="pictures/043-warm.png" alt="" className="icons" />
+          <strong>30°</strong> 21°
+        </div>
       </div>
-      <div className="col-2">
-        Wed <img src="pictures/038-cloudy-3.png" alt="" className="icons" />
-        <strong>19°</strong> 14°
-      </div>
-      <div className="col-2">
-        Thu <img src="pictures/011-cloudy.png" alt="" className="icons" />
-        <strong>21°</strong> 17°
-      </div>
-      <div className="col-2">
-        Fri <img src="pictures/034-cloudy-1.png" alt="" className="icons" />
-        <strong>24°</strong> 19°
-      </div>
-      <div className="col-2">
-        Sat <img src="pictures/039-sun.png" alt="" className="icons" />
-        <strong>26°</strong> 21°
-      </div>
-      <div className="col-2">
-        Sun <img src="pictures/043-warm.png" alt="" className="icons" />
-        <strong>30°</strong> 21°
-      </div>
-    </div>
-  );
+    );
+  }
 }
